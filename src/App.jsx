d0ad1130f64344d43a86ba8d5fc0319a6864ad3d
Muhammad Ramadhan <131851453+autumnleaf-ra@users.noreply.github.com>
2024-04-04 03:04:21 +0000

    add: react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import Cards from "./cards/Cards";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <div className="flex justify-center pb-2  bg-amber-600">
-        <div className="mt-5 text-xl font-semibold">Welcome to Pokedex</div>
-      </div>
-      <div className="flex justify-center mt-2 pb-2 space-x-10">
-        <a
-          href="#"
-          className="hover:underline decoration-amber-600 decoration-2"
-        >
-          Pokemon List
-        </a>
-        <a
-          href="#"
-          className="hover:underline decoration-amber-600 decoration-2"
-        >
-          My Pokedex
-        </a>
-      </div>
-      <div className="flex container mx-auto justify-center">
-        <div class="sm:grid grid-cols-2 gap-2 lg:grid-cols-4">
-          <Cards />
-          <Cards />
-          <Cards />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
-      </div>
+      </Router>
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Cards from "./cards/Cards";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex justify-center pb-2  bg-amber-600">
+        <div className="mt-5 text-xl font-semibold">Welcome to Pokedex</div>
+      </div>
+      <div className="flex justify-center mt-2 pb-2 space-x-10">
+        <a
+          href="#"
+          className="hover:underline decoration-amber-600 decoration-2"
+        >
+          Pokemon List
+        </a>
+        <a
+          href="#"
+          className="hover:underline decoration-amber-600 decoration-2"
+        >
+          My Pokedex
+        </a>
+      </div>
+      <div className="flex container mx-auto justify-center">
+        <div className="sm:grid grid-cols-2 gap-2 lg:grid-cols-4">
+          <Cards />
+          <Cards />
+          <Cards />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
