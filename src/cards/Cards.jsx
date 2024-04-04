a14c8b84caa7419da795e136eed39e197e94b670
Muhammad Ramadhan <131851453+autumnleaf-ra@users.noreply.github.com>
2024-04-04 02:47:21 +0000

    first commit

---
+++ src/cards/Cards.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <>
+      <div className="cards w-56">
+        <div className="flex border justify-center py-6">
+          <div className="flex flex-col space-x-4">
+            <div>
+              <img src="" alt="photos" />
+            </div>
+            <div>name : askdkas</div>
+            <div>abbilites: asjdjasjd</div>
+            <div>height : 91293921</div>
+          </div>
+        </div>
+      </div>
+      <div className="cards w-56">
+        <div className="flex border justify-center py-6">
+          <div className="flex flex-col space-x-4">
+            <div>
+              <img src="" alt="photos" />
+            </div>
+            <div>name : askdkas</div>
+            <div>abbilites: asjdjasjd</div>
+            <div>height : 91293921</div>
+          </div>
+        </div>
+      </div>
+      <div className="cards w-56">
+        <div className="flex border justify-center py-6">
+          <div className="flex flex-col space-x-4">
+            <div>
+              <img src="" alt="photos" />
+            </div>
+            <div>name : askdkas</div>
+            <div>abbilites: asjdjasjd</div>
+            <div>height : 91293921</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
